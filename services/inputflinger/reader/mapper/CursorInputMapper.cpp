82bbff4fe6af5ed7b43ebf7ac1a049913e74705e
nift4 <nift4@protonmail.com>
2022-08-13 12:26:35 +0200

    inputflinger: add absolute mouse input support
    Greatly enhances support for virtual desktop input!
    
    Change-Id: I3437065e04830cf225a496ed0fb51f8c4544e58e

--- services/inputflinger/reader/mapper/CursorInputMapper.cpp
+++ services/inputflinger/reader/mapper/CursorInputMapper.cpp
@@ -69,6 +69,41 @@ void CursorMotionAccumulator::finishSync() {
     clearRelativeAxes();
 }
 
+// --- CursorPositionAccumulator ---
+
+CursorPositionAccumulator::CursorPositionAccumulator() {
+    clearPos();
+    supported = false;
+}
+
+void CursorPositionAccumulator::reset(InputDeviceContext& deviceContext) {
+    clearPos();
+}
+
+void CursorPositionAccumulator::clearPos() {
+    mX = -1;
+    mY = -1;
+}
+
+void CursorPositionAccumulator::process(const RawEvent* rawEvent) {
+    if (rawEvent->type == EV_ABS) {
+        switch (rawEvent->code) {
+            case ABS_X:
+                supported = true;
+                mX = rawEvent->value;
+                break;
+            case ABS_Y:
+                supported = true;
+                mY = rawEvent->value;
+                break;
+        }
+    }
+}
+
+void CursorPositionAccumulator::finishSync() {
+    clearPos();
+}
+
 // --- CursorInputMapper ---
 
 CursorInputMapper::CursorInputMapper(InputDeviceContext& deviceContext,
@@ -210,6 +245,7 @@ std::list<NotifyArgs> CursorInputMapper::reset(nsecs_t when) {
 
     mCursorButtonAccumulator.reset(getDeviceContext());
     mCursorMotionAccumulator.reset(getDeviceContext());
+    mCursorPositionAccumulator.reset(getDeviceContext());
     mCursorScrollAccumulator.reset(getDeviceContext());
 
     return InputMapper::reset(when);
@@ -219,6 +255,7 @@ std::list<NotifyArgs> CursorInputMapper::process(const RawEvent* rawEvent) {
     std::list<NotifyArgs> out;
     mCursorButtonAccumulator.process(rawEvent);
     mCursorMotionAccumulator.process(rawEvent);
+    mCursorPositionAccumulator.process(rawEvent);
     mCursorScrollAccumulator.process(rawEvent);
 
     if (rawEvent->type == EV_SYN && rawEvent->code == SYN_REPORT) {
@@ -266,6 +303,13 @@ std::list<NotifyArgs> CursorInputMapper::sync(nsecs_t when, nsecs_t readTime) {
     // Rotate delta according to orientation.
     rotateDelta(mOrientation, &deltaX, &deltaY);
 
+    float absX = mCursorPositionAccumulator.getX() * mXScale;
+    float absY = mCursorPositionAccumulator.getY() * mYScale;
+    bool movedAbs = mCursorPositionAccumulator.isSupported() && absX > 0 && absY > 0;
+
+    // Rotate delta according to orientation.
+    rotateDelta(mOrientation, &absX, &absY);
+
     // Move the pointer.
     PointerProperties pointerProperties;
     pointerProperties.clear();
@@ -288,13 +332,13 @@ std::list<NotifyArgs> CursorInputMapper::sync(nsecs_t when, nsecs_t readTime) {
     float yCursorPosition = AMOTION_EVENT_INVALID_CURSOR_POSITION;
     if (mSource == AINPUT_SOURCE_MOUSE) {
         if (!mEnablePointerChoreographer) {
-            if (moved || scrolled || buttonsChanged) {
+            if (moved || movedAbs || scrolled || buttonsChanged) {
                 mPointerController->setPresentation(
                         PointerControllerInterface::Presentation::POINTER);
 
                 if (moved) {
                     mPointerController->move(deltaX, deltaY);
-                }
+                } else if (movedAbs) {
                 mPointerController->unfade(PointerControllerInterface::Transition::IMMEDIATE);
             }
 
@@ -320,7 +364,7 @@ std::list<NotifyArgs> CursorInputMapper::sync(nsecs_t when, nsecs_t readTime) {
     // the device in your pocket.
     // TODO: Use the input device configuration to control this behavior more finely.
     uint32_t policyFlags = 0;
-    if ((buttonsPressed || moved || scrolled) && getDeviceContext().isExternal()) {
+    if ((buttonsPressed || moved || movedAbs || scrolled) && getDeviceContext().isExternal()) {
         policyFlags |= POLICY_FLAG_WAKE;
     }
 
@@ -330,7 +374,7 @@ std::list<NotifyArgs> CursorInputMapper::sync(nsecs_t when, nsecs_t readTime) {
                                 currentButtonState);
 
     // Send motion event.
-    if (downChanged || moved || scrolled || buttonsChanged) {
+    if (downChanged || moved || movedAbs || scrolled || buttonsChanged) {
         int32_t metaState = getContext()->getGlobalMetaState();
         int32_t buttonState = lastButtonState;
         int32_t motionEventAction;
